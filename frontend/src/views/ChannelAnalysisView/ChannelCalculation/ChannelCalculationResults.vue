99ef8a72d5257e914606935356058e500411af56
729149195 <729149195@qq.com>
2025-03-08 23:09:02 +0800

    indexedDB long

--- frontend/src/views/ChannelAnalysisView/ChannelCalculation/ChannelCalculationResults.vue
+++ frontend/src/views/ChannelAnalysisView/ChannelCalculation/ChannelCalculationResults.vue
@@ -34,10 +34,6 @@ const fetchChannelData = async (channel) => {
         let channelData = channelDataCache.value[channelKey];
         if (!channelData) {
             const data = await store.dispatch('fetchChannelData', { channel });
-            // store.commit('updateChannelDataCache', {
-            //     channelKey: channelKey,
-            //     data: data
-            // });
             channelData = data; // 获取最新数据
         }
 
