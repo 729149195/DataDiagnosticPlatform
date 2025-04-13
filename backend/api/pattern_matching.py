0762cf92b87d140298f3ae29b69e7711155f23f3
729149195 <729149195@qq.com>
2025-04-14 05:00:38 +0800

    sketch message

--- backend/api/pattern_matching.py
+++ backend/api/pattern_matching.py
@@ -2,8 +2,8 @@ def match_pattern(normalized_query_pattern, channel_data_list):
     """
     在多个通道数据channel_data_list中查找与查询模式normalized_query_pattern匹配的部分
     Args:
-        normalized_query_pattern: 归一化后的查询模式点序列 [(x1, y1), (x2, y2), ...]
-        channel_data_list: 通道数据列表，每个元素包含通道信息和数据
+        normalized_query_pattern: 归一化并且平滑后的查询模式点序列 [(x1, y1), (x2, y2), ...]
+        channel_data_list: 通道数据列表，每个元素包含通道的原始信息和数据（数据量根据采样率的不同从几万到几百万不等）
         
         ===== 数据格式信息 =====
         normalized_query_pattern 类型: <class 'list'>
@@ -15,7 +15,6 @@ def match_pattern(normalized_query_pattern, channel_data_list):
         channel_data_list 元素类型: <class 'dict'>
         channel_data_list 结构示例:
         元素包含的键: ['channel_number', 'X_value', 'Y_value', 'X_unit', 'Y_unit', 'is_downsampled', 'is_upsampled', 'points', 'originalFrequency', 'stats', 'is_digital', 'Y_normalized', 'channel_name', 'shot_number']
-        其中X_value和Y_value是列表, 列表中每个元素是float, 数据量根据采样率的不同从几万到几百万不等
         ========================
         
     Returns:
